eec9e599a23a729f070e2e54a782b49f8de5fc61
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-06-26 11:31:16 -0400

    Format values

--- components/Layout.js
+++ components/Layout.js
@@ -1,44 +1,44 @@
 const Layout = ({children, fullWidth}) => (
   <div>
-    <header id="main-header" class="main-header" role="banner">
-      <div class="inner">
+    <header id="main-header" className="main-header" role="banner">
+      <div className="inner">
         <h1>
           <a href="https://www.ipp.eu/" title="retour à l’accueil">
-            <img src="https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo.png" width="224" alt="Institut des Politiques Publiques – IPP" srcset="https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo.png 1x, https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo@2x.png 2x"/>
+            <img src="https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo.png" width="224" alt="Institut des Politiques Publiques – IPP" srcSet="https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo.png 1x, https://www.ipp.eu/wp-content/themes/ipp/assets/img/ipp_logo@2x.png 2x"/>
           </a>
         </h1>
       </div>
-      <div class="mobile-handler">
-        <a href="#0" class="nav-trigger">
-          <span class="cd-label">Menu</span>
-          <span class="cd-icon"></span>
+      <div className="mobile-handler">
+        <a href="#0" className="nav-trigger">
+          <span className="cd-label">Menu</span>
+          <span className="cd-icon"></span>
         </a>
       </div>
-      <nav class="main-nav-container">
-        <div class="inner">
-          <ul id="main-nav" class="main-nav">
-            <li id="menu-item-192" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-192">
+      <nav className="main-nav-container">
+        <div className="inner">
+          <ul id="main-nav" className="main-nav">
+            <li id="menu-item-192" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-192">
               <a href="https://www.ipp.eu/presentation/">Accueil</a>
             </li>
-            <li id="menu-item-98" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-98">
+            <li id="menu-item-98" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-98">
               <a href="https://www.ipp.eu/presentation/presentation/">L’Institut</a>
             </li>
-            <li id="menu-item-99" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99">
+            <li id="menu-item-99" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-99">
               <a href="https://www.ipp.eu/presentation/chercheurs/">Chercheurs</a>
             </li>
-            <li id="menu-item-100" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-100">
+            <li id="menu-item-100" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-100">
               <a href="https://www.ipp.eu/presentation/thematiques/">Thématiques</a>
             </li>
-            <li id="menu-item-189" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-189">
+            <li id="menu-item-189" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-189">
               <a href="https://www.ipp.eu/presentation/methodes/">Méthodes</a>
             </li>
-            <li id="menu-item-101" class="active">
-              <a href="https://www.ipp.eu/presentation/outils/" class="active">Outils</a>
+            <li id="menu-item-101" className="active">
+              <a href="https://www.ipp.eu/presentation/outils/" className="active">Outils</a>
             </li>
-            <li id="menu-item-5565" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-5565">
+            <li id="menu-item-5565" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-5565">
               <a href="https://www.ipp.eu/presentation/publications/">Publications</a>
             </li>
-            <li id="menu-item-104" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-104">
+            <li id="menu-item-104" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-104">
               <a href="https://www.ipp.eu/actualites/">Actualités</a>
             </li>""
           </ul>
--- components/Table.js
+++ components/Table.js
@@ -8,6 +8,10 @@ import last from 'lodash.last'
 
 import ReactTable from 'react-table'
 
+import { FormattedDate, IntlProvider, addLocaleData, FormattedNumber} from 'react-intl'
+import fr from 'react-intl/locale-data/fr'
+addLocaleData(fr)
+
 function preprocess(tableData) {
   const dates = union(...map(tableData, param => keys(param.values))).sort()
   return dates.reduce((data, date) => {
@@ -24,21 +28,27 @@ function preprocess(tableData) {
   }, [])
 }
 
-function buildSimpleColumn(openfiscaKey, description) {
+const cellFormatter = {
+  '/1': props => props.value !== undefined && <FormattedNumber value={props.value} style="percent" maximumFractionDigits={3}/>,
+  'EUR': props => props.value !== undefined && <FormattedNumber value={props.value} style="currency" maximumFractionDigits={3} currency="EUR"/>,
+}
+
+function buildSimpleColumn(openfiscaKey, parameter) {
   return {
-    Header: description,
+    Header: parameter.description,
     accessor: item => item[openfiscaKey],
-    id: openfiscaKey
+    id: openfiscaKey,
+    Cell: cellFormatter[parameter.unit]
   }
 }
 
 function buildColumns(tableDesc, tableData) {
   if (isString(tableDesc)) {
-    return [buildSimpleColumn(tableDesc, tableData[tableDesc].description)]
+    return [buildSimpleColumn(tableDesc, tableData[tableDesc])]
   }
   return map(tableDesc, (nodeDesc, description) => {
     if (isString(nodeDesc)) {
-      return buildSimpleColumn(nodeDesc, description)
+      return buildSimpleColumn(nodeDesc, Object.assign({}, tableData[nodeDesc], {description}))
     }
     return {
       Header: description,
@@ -51,16 +61,20 @@ const Table = ({desc, data}) => {
   const dateColumn = {
     Header: 'Date d’effet',
     accessor: 'date',
+    Cell: props => <FormattedDate value={props.value}/>
   }
   const columns = [dateColumn].concat(buildColumns(desc, data))
-  return <ReactTable
-    data={preprocessedData}
-    columns={columns}
-    showPagination={false}
-    defaultPageSize={preprocessedData.length}
-    className="-striped -highlight"
-    defaultSorted={[{id: 'date', desc: true}]}
-  />
+  return (<IntlProvider locale="fr">
+      <ReactTable
+        data={preprocessedData}
+        columns={columns}
+        showPagination={false}
+        defaultPageSize={preprocessedData.length}
+        className="-striped -highlight"
+        defaultSorted={[{id: 'date', desc: true}]}
+      />
+    </IntlProvider>
+    )
 }
 
 export default Table
