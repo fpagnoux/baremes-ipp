1ef0f43f84cc265371df94e94087a1b5932f918c
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-02-08 10:35:00 -0500

    Properly format AF

--- services/formatter.js
+++ services/formatter.js
@@ -5,9 +5,17 @@ Intl.NumberFormat = IntlPolyfill.NumberFormat;
 Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
 
 export function formatNumber(value, options) {
+  if (options && options.currency == 'AFRF') {
+    return formatAFRF(value, options)
+  }
   return (new Intl.NumberFormat('fr', options)).format(value)
 }
 
 export function formatDate(value) {
   return new Intl.DateTimeFormat('fr', {timeZone: 'UTC'}).format(new Date(value))
+}
+
+function formatAFRF(value, options) {
+    const {currency, ...otherOptions} = options
+    return formatNumber(value, {currency: 'FRF', ...otherOptions}).replace('F', 'AF')
 }
