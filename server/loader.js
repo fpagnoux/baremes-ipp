31d9aad62ba71dfbd7cbd1e839cfc1115c182266
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-02-11 18:20:13 -0500

    Split paramter loading from route generating

--- server/loader.js
+++ server/loader.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const yaml = require('js-yaml');
 const map = require('lodash.map')
 const flatten = require('lodash.flatten')
+const fromPairs = require('lodash.frompairs')
+
 
 const resolver = require('./resolver')
 
@@ -12,12 +14,18 @@ async function loadSectionFile(file) {
   const textContent = fs.readFileSync(`./tables/${file}`, 'utf8')
   const yamlContent = yaml.safeLoad(textContent)
   const resolvedDesc = await resolver.resolveSection(yamlContent)
-  return extractRoutes(resolvedDesc, `/${fileName}`)
+  return [fileName, resolvedDesc]
+}
+
+async function loadParametersTree() {
+  const sectionsFiles = fs.readdirSync('./tables')
+  const resolvedFiles = await Promise.all(sectionsFiles.map(loadSectionFile))
+  return fromPairs(resolvedFiles)
 }
 
 async function loadRoutes() {
-  const sectionsFiles = fs.readdirSync('./tables')
-  const routes = await Promise.all(sectionsFiles.map(loadSectionFile))
+  const parametersTree = await loadParametersTree()
+  const routes = map(parametersTree, ((content, fileName) => extractRoutes(content, `/${fileName}`)))
   return flatten(routes)
 }
 
