3f26fc419810dcba481c4511d4a3df4962c31b0b
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-02-11 11:58:35 -0500

    fixup! Aligne les sections sur les noms de Wordpress

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ const Index = () => (
       <h4>Prélèvements obligatoires</h4>
       <ul>
         <li><a href="./prelevements-sociaux">Prélèvements sociaux</a></li>
-        <li><a href="./impot-revenu">Impôt sur le revenu</a></li>
-        <li><a href="./taxation-capital">Taxation du capital</a></li>
+        <li><a href="./impot-sur-le-revenu">Impôt sur le revenu</a></li>
+        <li><a href="./taxation-du-capital">Taxation du capital</a></li>
         <li><a href="./taxation-indirecte">Taxation indirecte</a></li>
         <li><a href="./taxation-societes">Taxation des entreprises</a></li>
       </ul>
@@ -17,12 +17,12 @@ const Index = () => (
       <ul>
         <li><a href="./prestations-sociales">Prestations sociales</a></li>
         <li><a href="./chomage">Chômage</a></li>
-        <li><a href="./retraite">Retraite</a></li>
+        <li><a href="./regimes-de-retraites">Retraite</a></li>
       </ul>
       <h4>Réglementations</h4>
       <ul>
-        <li><a href="./marche-travail">Marché du travail</a></li>
-        <li><a href="./tarifs-energie">Tarifs réglementés de l’énergie</a></li>
+        <li><a href="./marche-du-travail">Marché du travail</a></li>
+        <li><a href="./tarifs-reglementes-energie">Tarifs réglementés de l’énergie</a></li>
       </ul>
     </div>
   </Layout>
