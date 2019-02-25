eca4741818156b9c9d42b2cd5f8586645b6ba65a
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-02-25 16:21:24 -0500

    Generate tables dynamically

---
+++ __tests__/services/csv.js
@@ -0,0 +1,20 @@
+import {toCSV, toXLSX, cleanDatum, generateTables} from '../../services/csv'
+import rsaMajorationTable from '../assets/rsa_maj_table.json'
+import rsaMajoration from '../services/rsa_maj.json'
+
+it('should clean a datum', () => {
+  const result = cleanDatum(rsaMajoration[0], rsaMajorationId)
+  expect(result['majoration_rsa.taux_personne_supp']).toEqual(0.4)
+});
+
+
+it('should convert to CSV', () => {
+  const result = toCSV(rsaMajoration, rsaMajorationId)
+  expect(result).toContain('2009-06-20,0.5,0.3,0.4,1.28412,0.42804')
+});
+
+// it('should create a XLSX file', (done) => {
+//   const result = toXLSX(rsaMajorationTable, rsaMajoration.id)
+//   result.xlsx.writeFile('/Users/florianpagnoux/Desktop/test.xlsx')
+//     .then(() => done());
+// })
--- pages/table.js
+++ pages/table.js
@@ -1,5 +1,6 @@
 import {withRouter} from 'next/router'
 import last from 'lodash.last'
+import FileSaver from 'file-saver'
 
 import msg from '../messages'
 import {basename, csvPath, isProd} from '../config'
@@ -7,9 +8,42 @@ import Table from "../components/Table"
 import Layout from '../components/Layout'
 import LangToggle from '../components/LangToggle'
 import {getTitle} from '../services/i18n'
+import {parameterTable} from '../services/parameterTable'
+import {toCSV, toXLSX} from '../services/csv'
 
 function getLinkToTable(parameter, path, format) {
-  return `${csvPath || basename}${path}/${last(parameter.id.split('.'))}.${format}`
+  if (isProd) { // In prod, tables are statically generated
+    return `${csvPath || basename}${path}/${last(parameter.id.split('.'))}.${format}`
+  }
+}
+
+const CSVLink = ({path, parameter, table}) => {
+  const fileName = `${last(parameter.id.split('.'))}.csv`
+  if (isProd) { // In prod, tables are statically generated
+    const target = `${csvPath || basename}${path}/${fileName}`
+    return <a href={target}>CSV</a>
+  }
+  return <a href="#" onClick={() => {
+    const csv = toCSV(table.data, parameter.id)
+    const blob = new Blob([content], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, fileName)
+  }}>CSV</a>
+}
+
+const XSLXLink = ({path, parameter, table}) => {
+  const tableName = last(parameter.id.split('.'))
+  const fileName = `${tableName}.xlsx`
+  if (isProd) { // In prod, tables are statically generated
+    const target = `${csvPath || basename}${path}/${fileName}`
+    return <a href={target}>XSLX</a>
+  }
+  return <a href="#" onClick={() => {
+    const xlsx = toXLSX(table, tableName)
+    xlsx.xlsx.writeBuffer().then(data => {
+        const blob = new Blob([data], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+        FileSaver.saveAs(blob, `${tableName}.xlsx`)
+      })
+  }}>XSLX</a>
 }
 
 const BreadCrum = ({parents, lang}) => {
@@ -29,12 +63,13 @@ const BreadCrum = ({parents, lang}) => {
 const TablePage = (props) => {
   const {parameter, parents, lang} = props.router.query
   const path = props.router.asPath
+  const table = parameterTable(parameter, lang)
   return <Layout fullWidth={ true }>
     <BreadCrum parents={parents} lang={lang}/>
     {! isProd && <LangToggle lang={lang} path={path}/>}
     <h1 className="box"><span>{getTitle(parameter, lang)}</span></h1>
-    <a href={getLinkToTable(parameter, path, 'csv')}>CSV</a> <a href={getLinkToTable(parameter, path, 'xlsx')}>XSLX</a>
-    <Table parameter={parameter} lang={lang}/>
+    <CSVLink path={path} parameter={parameter} table={table}/> <XSLXLink path={path} parameter={parameter} table={table}/>
+    <Table table={table}/>
     <p className="table-doc">{parameter.documentation}</p>
   </Layout>
   }
